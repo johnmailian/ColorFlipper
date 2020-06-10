0258f02920d9d3c22b17c7ac2dedb017557849f9
Hovhasnnes Mayilyan <johnmailian@gmail.com>
2020-06-09 19:47:45 -0700

    Vanilla JS Color Flipper

---
+++ main.js
@@ -0,0 +1,18 @@
+const colors = ['green', 'blue', 'yellow', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+
+    // get random number between 0 and 3
+    const randomNumber = getRandomColor();
+    console.log(randomNumber);
+    
+    document.body.style.backgroundColor = colors[randomNumber] ;
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomColor(){
+    return Math.floor(Math.random()*colors.length);
+}
